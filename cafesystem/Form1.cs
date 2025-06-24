11d22f6960aa7506224b2890e6c50ae05d4a1365
hananizz <hananideyja05@gmail.com>
2025-06-25 00:23:23 +0800

    add delivery

--- cafesystem/Form1.cs
+++ cafesystem/Form1.cs
@@ -16,5 +16,10 @@ namespace cafesystem
         {
             InitializeComponent();
         }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
